228ef9ad7059310f42456e3c4c4be7a1677ca893
Snigdha Datta <116970908+SnigdhaDatta@users.noreply.github.com>
2025-07-26 00:21:19 +0530

    feat:  style changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Footer from "@/components/footer";
 import Header from "@/components/header";
+import Sidebar from "@/components/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +26,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en"
-     className="hydrated">
+    <html lang="en" className="hydrated">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-br from-[#fbfbfb] via-[#fdeff4] to-[#ffe6e6] min-h-screen bg-fixed`}
+        className={`${geistSans.variable} ${geistMono.variable} min-h-screen flex flex-col bg-gradient-to-br from-[#fbfbfb] via-[#fdeff4] to-[#ffe6e6]`}
       >
-        <Header></Header>
-        {children}
+        <Header />
+        <div className="flex flex-1 w-full">
+          <Sidebar />
+          <div className="flex flex-1 flex-col items-center justify-between max-h-screen">
+            {/* Main Content */}
+            <main className="w-full max-w-3xl flex-grow px-4 py-8">
+              {children}
+            </main>
+            {/* Footer */}
+            <Footer />
+          </div>
+        </div>
       </body>
     </html>
   );
