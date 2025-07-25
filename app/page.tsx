6a8d7f4355e9f41e64d0bc8146f2f38eeb22ccd6
Snigdha Datta <116970908+SnigdhaDatta@users.noreply.github.com>
2025-07-26 00:27:17 +0530

    feat: mascot addition and image resizing

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,35 @@
 import Image from "next/image";
 import InputWithButton from "@/components/searchbar";
-
 export default function Home() {
   return (
-    <div className="auto flex justify-center mt-16">
-        <div className="text-center">
-          {/* Logo + Heading */}
-          <div className="flex items-center justify-center gap-3 mb-6 transform transition-transform duration-300 hover:scale-[0.97]">
-            <Image src="/icon.png" alt="Logo" width={50} height={40} />
-            <h1 className= "text-5xl font-bold text-[#d36f96] font-serif" >DramaDojo</h1>
-          </div>
+    <div className="flex flex-grow flex-col items-center m-16 mx-auto overflow-y-hidden">
+      {/* Logo + Heading */}
+      <div className="flex gap-3 mb-6 transform transition-transform duration-300 hover:scale-[0.97]">
+        <Image
+          src="/icon.png"
+          alt="Logo"
+          width={40}
+          height={30}
+          className="auto"
+        />
+        <h1 className="sm:text-5xl text-4xl text font-bold text-[#d36f96] font-serif">
+          DramaDojo
+        </h1>
+      </div>
 
-          {/* Search Bar */}
-          <InputWithButton />
-        </div>
+      {/* Search Bar */}
+      <InputWithButton />
+
+      {/*mascot of home page */}
+      <div className="mt-10 flex justify-center">
+        <Image
+          src="/tanjiro.png"
+          alt="Mascot"
+          width={260}
+          height={260}
+          className="drop-shadow-lg hover:scale-105 transition-transform duration-300"
+        />
+      </div>
     </div>
-
-
   );
 }
